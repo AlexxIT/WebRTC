922395d1c7ce5552f0eb2cfc7fd05e9b54d0be32
Alexey Khit <alexey.khit@gmail.com>
2023-01-18 15:31:17 +0300

    Update go2rtc version to v0.1-rc.9

--- custom_components/webrtc/utils.py
+++ custom_components/webrtc/utils.py
@@ -23,7 +23,7 @@ _LOGGER = logging.getLogger(__name__)
 
 DOMAIN = "webrtc"
 
-BINARY_VERSION = "v0.1-rc.8"
+BINARY_VERSION = "v0.1-rc.9"
 
 SYSTEM = {
     "Windows": {"AMD64": "go2rtc_win64.zip"},
