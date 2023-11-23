afef9d7c9be3dcf5c59ddb601e7f912c40283a0c
Fuat Akgün <fuatakgun@gmail.com>
2023-11-23 20:09:04 +0100

    Update go2rtc version to 1.8.4

--- custom_components/webrtc/utils.py
+++ custom_components/webrtc/utils.py
@@ -24,7 +24,7 @@ _LOGGER = logging.getLogger(__name__)
 
 DOMAIN = "webrtc"
 
-BINARY_VERSION = "1.8.1"
+BINARY_VERSION = "1.8.4"
 
 SYSTEM = {
     "Windows": {"AMD64": "go2rtc_win64.zip", "ARM64": "go2rtc_win_arm64.zip"},
