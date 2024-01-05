03a07f9cae771b5c5771f9fc5f9679631bfb2faf
Alex X <alexey.khit@gmail.com>
2024-01-05 17:14:42 +0300

    Update minimum Hass version to 2023.7.0

--- tests/test_backward.py
+++ tests/test_backward.py
@@ -1,5 +1,10 @@
+from homeassistant.components.image import ImageEntity
 from homeassistant.components.media_player import MediaPlayerEntityFeature
 
 
 def test_2022_5_0():
     assert MediaPlayerEntityFeature
+
+
+def test_2023_7_0():
+    assert ImageEntity
